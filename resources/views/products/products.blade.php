00e811f739bbeda2cb71d9cd99711dd184c6100e
golub1989 <tomislav.golubicic@fpmoz.sum.ba>
2023-08-19 18:22:20 +0200

    Display product's images

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -1,10 +1,18 @@
+@extends('welcome')
+
+@section('content')
 @foreach($products as $product)
     {{$product->name}}
     {{$product->description}}
     {{$product->price}}
-    {{$product->image}}
+    <!-- {{$product->image}} -->
+    <img src="{{ asset('/storage/images/'.$product->image) }}" style="height: 100px;width:100px;">
+
 
     <!-- <a href="{{ url('product/{id}') }}">View</a> -->
     <a href="{{ url('product' , [ 'id' => $product->id ]) }}">View</a>
 
 @endforeach
+@endsection
+
+
